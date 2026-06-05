ef04961928627243e3d3a13223b982d0ad710161
Mahliyo <gasandigital13@gmail.com>
2026-06-05 23:40:23 +0500

    My project header

--- My project/src/Components/Header/Header.jsx
+++ My project/src/Components/Header/Header.jsx
@@ -1,8 +1,9 @@
+import { useTranslation } from "react-i18next";
 import "./Header.css";
 import React from 'react'
 
 function Header() {
-    const [t, i18n] =
+    const [t, i18n] = useTranslation();
   return (
     <div>Header</div>
   )
