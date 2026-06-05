287defac59e067b91aca4343c54075cdc354442d
Mahliyo <gasandigital13@gmail.com>
2026-06-05 23:37:36 +0500

    My project header

--- My project/src/App.jsx
+++ My project/src/App.jsx
@@ -1,9 +1,12 @@
 import "./App.css";
 import React from 'react'
+import Header from "./Components/Header/Header";
 
 function App() {
   return (
-    <div>App</div>
+    <div>
+      <Header/>
+    </div>
   )
 }
 
